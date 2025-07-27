4fac887edb5a54c99c1cc7dfd625634efb98fd30
Ove Klinghed <ove.klinghed@gmail.com>
2025-07-27 20:59:56 +0200

    Mode

--- src/main/java/se/klinged/gateway/route/Application.java
+++ src/main/java/se/klinged/gateway/route/Application.java
@@ -1,4 +1,4 @@
-package se.ehalsomyndigheten.gateway.route;
+package se.klinghed.gateway.route;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
