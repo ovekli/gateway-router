e21a629f69c11c3105456b2846aee9171634ecd1
Ove Klinghed <ove.klinghed@gmail.com>
2025-08-05 19:06:33 +0200

    Mod

--- src/main/java/se/klinged/gateway/route/Application.java
+++ src/main/java/se/klinged/gateway/route/Application.java
@@ -3,9 +3,11 @@ package se.klinghed.gateway.route;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @RestController
+@EnableDiscoveryClient
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
